4d6467c011cf079f66194a2c9c8f2f1621f87bca
Jack Kenlay <jackkenlay@hw034532.home>
2019-08-26 21:17:34 +0100

    WIP

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const main = require('./controller/main.js');
 
 
 function init () {
+  console.log('Lei is a fine piece of arse ;) ;) ;) ;)');
   main.start();
 }
+
+
 init();
 //establish connection to db?
 //start doing the model with MySql
